f2958f69724173ae1a9903dd4fb0ff07ee88676c
Metin Balaban <metin@Metins-MacBook-Pro.local>
2018-10-11 03:01:04 -0700

    HYBRID algo is slightly modified. It enforces positivity constraint only in the first stage. Also some refactoring

--- least_squares_placement.py
+++ least_squares_placement.py
@@ -5,6 +5,8 @@ from abc import ABC, abstractmethod
 import heapq
 import math
 import re
+
+
 # Glossary
 # OLS: Ordinary Least Squares
 # FM: Fitch-Margoliash --Least Squares with Variance scaling
@@ -18,24 +20,27 @@ class Algorithm(ABC):
     def placement_per_edge(self):
         pass
 
-    @abstractmethod
-    def error_per_edge(self, edge):
+    @staticmethod
+    def error_per_edge(edge):
         pass
 
     def placement(self, query_name, selection_name):
         if selection_name == "HYBRID":
             sm = heapq.nsmallest(math.floor(math.log2(len(self.tree.leaf_nodes()))),
-                                 tree.postorder_edge_iter(filter_fn = lambda e: e.head_node != tree.seed_node),
-                                 key = lambda e: self.error_per_edge(e))
-            placed_edge = min(sm, key = lambda e: e.x_1)
+                                 tree.postorder_edge_iter(filter_fn=lambda e: e.head_node != tree.seed_node),
+                                 key=lambda e: self.error_per_edge(e))
+            placed_edge = min(sm, key=lambda e: e.x_1_neg)
+            insert(placed_edge, query_name, placed_edge.x_1_neg, placed_edge.x_2_neg)
         elif selection_name == "ME":
-            placed_edge = min(tree.postorder_edge_iter(filter_fn = lambda e: e.head_node != tree.seed_node),
-                              key=lambda e: e.x_1)
-        else: # selection_name == "MLSE"
-            placed_edge = min(tree.postorder_edge_iter(filter_fn = lambda e: e.head_node != tree.seed_node),
+            placed_edge = min(tree.postorder_edge_iter(filter_fn=lambda e: e.head_node != tree.seed_node),
+                              key=lambda e: e.x_1_neg)
+            insert(placed_edge, query_name, placed_edge.x_1_neg, placed_edge.x_2_neg)
+        else:  # selection_name == "MLSE"
+            placed_edge = min(tree.postorder_edge_iter(filter_fn=lambda e: e.head_node != tree.seed_node),
                               key=lambda e: self.error_per_edge(e))
-        insert(placed_edge, query_name)
+            insert(placed_edge, query_name, placed_edge.x_1, placed_edge.x_2)
         return self.tree
+
 
 class OLS(Algorithm):
 
@@ -50,12 +55,11 @@ class OLS(Algorithm):
             a_22 = a_11
             c_1 = e.RD + e.SD - e.length * e.S - e.Rd - e.Sd
             c_2 = e.RD - e.SD + e.length * e.S - e.Rd + e.Sd
-            x_1, x_2 = solve2_2(a_11, a_12, a_21, a_22, c_1, c_2, e.length)
-            e.x_1 = x_1
-            e.x_2 = x_2
+            solve2_2(e, a_11, a_12, a_21, a_22, c_1, c_2)
 
     # computes OLS error (Q value) for a given edge
-    def error_per_edge(self, edge):
+    @staticmethod
+    def error_per_edge(edge):
         A = edge.RD2 + edge.SD2
         B = 2 * (edge.x_1 + edge.x_2) * edge.Rd + \
             2 * (edge.length + edge.x_1 - edge.x_2) * edge.Sd
@@ -66,6 +70,7 @@ class OLS(Algorithm):
         E = -2 * edge.RDd - 2 * edge.SDd
         F = edge.Rd2 + edge.Sd2
         return A + B + C + D + E + F
+
 
 class FM(Algorithm):
 
@@ -80,12 +85,11 @@ class FM(Algorithm):
             a_22 = a_11
             c_1 = e.R1_D + e.S1_D - e.length * e.S1_D2 - e.Rd_D2 - e.Sd_D2
             c_2 = e.R1_D - e.S1_D + e.length * e.S1_D2 - e.Rd_D2 + e.Sd_D2
-            x_1, x_2 = solve2_2(a_11, a_12, a_21, a_22, c_1, c_2, e.length)
-            e.x_1 = x_1
-            e.x_2 = x_2
+            solve2_2(e, a_11, a_12, a_21, a_22, c_1, c_2)
 
     # computes Fitch-Margoliash error (Q value) for a given edge
-    def error_per_edge(self, edge):
+    @staticmethod
+    def error_per_edge(edge):
         A = edge.R + edge.S
         B = 2 * (edge.x_1 + edge.x_2) * edge.Rd_D2 + \
             2 * (edge.length + edge.x_1 - edge.x_2) * edge.Sd_D2
@@ -96,6 +100,7 @@ class FM(Algorithm):
         E = -2 * edge.Rd_D - 2 * edge.Sd_D
         F = edge.Rd2_D2 + edge.Sd2_D2
         return A + B + C + D + E + F
+
 
 class BE(Algorithm):
 
@@ -110,12 +115,11 @@ class BE(Algorithm):
             a_22 = a_11
             c_1 = e.R + e.S - e.length * e.S1_D - e.Rd_D - e.Sd_D
             c_2 = e.R - e.S + e.length * e.S1_D - e.Rd_D + e.Sd_D
-            x_1, x_2 = solve2_2(a_11, a_12, a_21, a_22, c_1, c_2, e.length)
-            e.x_1 = x_1
-            e.x_2 = x_2
+            solve2_2(e, a_11, a_12, a_21, a_22, c_1, c_2)
 
     # computes BE error (Q value) for a given edge
-    def error_per_edge(self, edge):
+    @staticmethod
+    def error_per_edge(edge):
         A = edge.RD + edge.SD
         B = 2 * (edge.x_1 + edge.x_2) * edge.Rd_D + \
             2 * (edge.length + edge.x_1 - edge.x_2) * edge.Sd_D
@@ -127,9 +131,10 @@ class BE(Algorithm):
         F = edge.Rd2_D + edge.Sd2_D
         return A + B + C + D + E + F
 
+
 # inserts query into reference tree at given edge.
 
-def insert(placed_edge, query_name):
+def insert(placed_edge, query_name, x_1, x_2):
     tailn = placed_edge.tail_node
     headn = placed_edge.head_node
     tailn.remove_child(headn)
@@ -137,42 +142,50 @@ def insert(placed_edge, query_name):
     nn.add_child(headn)
     qry = dy.Node(taxon=dy.Taxon(query_name))
     nn.add_child(qry)
-    qry.edge_length = placed_edge.x_1
+    qry.edge_length = x_1
     tailn.add_child(nn)
     if placed_edge.head_node in list(master_edge.head_node.ancestor_iter()) or master_edge == placed_edge:
-        nn.edge_length = placed_edge.length - max(placed_edge.x_2, 0)
-        headn.edge_length = max(placed_edge.x_2, 0)
+        nn.edge_length = placed_edge.length - max(x_2, 0)
+        headn.edge_length = max(x_2, 0)
     else:
-        nn.edge_length = max(placed_edge.x_2, 0)
-        headn.edge_length = placed_edge.length - max(placed_edge.x_2, 0)
+        nn.edge_length = max(x_2, 0)
+        headn.edge_length = placed_edge.length - max(x_2, 0)
+
 
 # solves two by two Ax=c linear system and returns the optimal values defined by
 # constraints. This is thanks to the convexity of (weighted) least squared error
-def solve2_2(a_11, a_12, a_21, a_22, c_1, c_2, edge_length):
+def solve2_2(e, a_11, a_12, a_21, a_22, c_1, c_2):
+    edge_length = e.length
     det = 1 / (a_11 * a_22 - a_12 * a_21)
     assert det is not 0
-    x_1 = (a_22 * c_1 - a_12 * c_2) * det
-    x_2 = (- a_21 * c_1 + a_11 * c_2) * det
+    x_1_neg = (a_22 * c_1 - a_12 * c_2) * det
+    x_2_neg = (- a_21 * c_1 + a_11 * c_2) * det
+    x_1 = x_1_neg
+    x_2 = x_2_neg
     if positive_branch:
-        if x_1 < 0 and x_2 < 0:
+        if x_1_neg < 0 and x_2_neg < 0:
             x_1 = 0
             x_2 = 0
-        elif x_1 > 0 and x_2 < 0:
-            x_1 = max(c_1 * 1.0 / a_11,0)
+        elif x_1_neg > 0 and x_2_neg < 0:
+            x_1 = max(c_1 * 1.0 / a_11, 0)
             x_2 = 0
-        elif x_1 < 0 and 0 <= x_2 and x_2 <= edge_length:
+        elif x_1_neg < 0 and 0 <= x_2_neg and x_2_neg <= edge_length:
             x_1 = 0
             x_2 = min(max(c_2 * 1.0 / a_22, 0), edge_length)
-        elif x_1 < 0 and x_2 > edge_length:
+        elif x_1_neg < 0 and x_2_neg > edge_length:
             x_1 = 0
             x_2 = edge_length
-        elif x_1 > 0 and x_2 > edge_length:
-            x_1 = max((c_1 * 1.0 - a_12 * edge_length) / a_11,0)
+        elif x_1_neg > 0 and x_2_neg > edge_length:
+            x_1 = max((c_1 * 1.0 - a_12 * edge_length) / a_11, 0)
             x_2 = edge_length
         else:
             x_1 = x_1
             x_2 = x_2
-    return (x_1, x_2)
+    e.x_1 = x_1
+    e.x_2 = x_2
+    e.x_1_neg = x_1_neg
+    e.x_2_neg = x_2_neg
+
 
 def dfs_S_values(edge, downstream):
     if downstream.is_leaf():
@@ -186,15 +199,16 @@ def dfs_S_values(edge, downstream):
         edge.Sd2_D2 = 0
         edge.SD = obs_dist[downstream.taxon.label]
         edge.SD2 = edge.SD * edge.SD
-        edge.S1_D = 1/edge.SD
-        edge.S1_D2 = 1/(edge.SD*edge.SD)
+        edge.S1_D = 1 / edge.SD
+        edge.S1_D2 = 1 / (edge.SD * edge.SD)
 
 
     else:
         inc = list(downstream.incident_edges())
         inc = filter(lambda e: e.head_node != tree.seed_node and e != edge, inc)
-        edge.S, edge.SDd, edge.Sd, edge.Sd_D, edge.Sd_D2, edge.Sd2, edge.Sd2_D, edge.Sd2_D2, edge.SD2, edge.SD, edge.S1_D,  edge.S1_D2 = 12*[0]
-        for d1  in inc:
+        edge.S, edge.SDd, edge.Sd, edge.Sd_D, edge.Sd_D2, edge.Sd2, edge.Sd2_D, edge.Sd2_D2, edge.SD2, edge.SD, edge.S1_D, edge.S1_D2 = 12 * [
+            0]
+        for d1 in inc:
             d1tips = [d1.head_node, d1.tail_node]
             d1tips.remove(downstream)
             if len(d1tips) == 0:
@@ -226,8 +240,8 @@ def dfs_R_values(edge, u1, upstream, downstream):
         edge.Rd2_D2 = 0
         edge.RD = obs_dist[upstream.taxon.label]
         edge.RD2 = edge.RD * edge.RD
-        edge.R1_D = 1/edge.RD
-        edge.R1_D2 = 1/(edge.RD*edge.RD)
+        edge.R1_D = 1 / edge.RD
+        edge.R1_D2 = 1 / (edge.RD * edge.RD)
 
     else:
         edge.R = u1.R
@@ -273,13 +287,12 @@ if __name__ == "__main__":
                       help="path to the reference tree", metavar="FILE")
     parser.add_option("-d", "--distances", dest="dist_fp",
                       help="path to the table of observed distances", metavar="FILE")
-    parser.add_option("-a", "--algo", dest="algo_name", default = "OLS",
+    parser.add_option("-a", "--algo", dest="algo_name", default="OLS",
                       help="name of the algorithm (OLS, FM, or BE)", metavar="ALGO")
-    parser.add_option("-s", "--selection", dest="selection_name", default = "MLSE",
+    parser.add_option("-s", "--selection", dest="selection_name", default="MLSE",
                       help="name of the placement selection criteria (MLSE, ME, or HYBRID", metavar="CRITERIA")
-    parser.add_option("-p", "--positive", dest="positive_branch", action = 'store_true',
+    parser.add_option("-p", "--positive", dest="positive_branch", action='store_true',
                       help="enforces positivity constraint on new branch lengths")
-
 
     (options, args) = parser.parse_args()
     tree_fp = options.tree_fp
@@ -287,8 +300,6 @@ if __name__ == "__main__":
     algo_name = options.algo_name
     selection_name = options.selection_name
     positive_branch = options.positive_branch
-
-
 
     f = open(tree_fp)
     tree_string = f.readline()
@@ -299,16 +310,14 @@ if __name__ == "__main__":
     for line in tbl.readlines():
         dists = list(re.split("\s+", line.strip()))
         query_name = dists[0]
-        obs_dist = dict(zip(tags, map(float,dists[1:])))
+        obs_dist = dict(zip(tags, map(float, dists[1:])))
         tree = dy.Tree.get_from_string(tree_string, schema='newick')
         master_edge = next(tree.postorder_edge_iter())
         for l in tree.leaf_node_iter():
             if l.taxon.label not in obs_dist:
                 raise ValueError('Taxon {} should be in distances table.'.format(l.taxon.label))
             if obs_dist[l.taxon.label] == 0:
-                l.edge.x_1 = 0
-                l.edge.x_2 = 0
-                insert(l.edge, query_name)
+                insert(l.edge, query_name, 0, 0)
                 tree.write(file=sys.stdout, schema="newick")
                 exit(0)
         dfs_S_values(master_edge, master_edge.tail_node)
@@ -321,5 +330,4 @@ if __name__ == "__main__":
             alg = OLS(tree)
         alg.placement_per_edge()
         output_tree = alg.placement(query_name, selection_name)
-        output_tree.write(file = sys.stdout, schema = "newick")
-
+        output_tree.write(file=sys.stdout, schema="newick")
