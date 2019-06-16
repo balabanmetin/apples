e56ffa047a9d020c16ec13277bc8471cdba79443
Metin Balaban <metin@Metins-MacBook-Pro.local>
2019-06-15 22:58:30 -0700

    distance calculation is now vectorized

--- apples/distance.py
+++ apples/distance.py
@@ -1,24 +1,17 @@
 import numpy as np
 
-def jc69(a,b):
-    mm = 0
-    valid = 0
-    for c1,c2 in zip(a,b):
-        if c1 == "-" or c2 == "-":
-            continue
-        else:
-            valid+=1
-            if c1 != c2:
-                mm += 1
-    if not valid :
-        return (5.0)
-    p = mm*1.0/valid
-    if (abs(p) - np.finfo(float).eps < 0):
+
+def jc69(a2,b2):
+    nondash=np.logical_and(a2!='-',b2!='-')
+    valid = np.count_nonzero(nondash)
+    if not valid:
+        return 5.0
+    p = np.count_nonzero(np.logical_and(a2!=b2, nondash))*1.0/valid
+    if (p - np.finfo(float).eps < 0):
         return 0.0
-
-    loc = 1 - (4 * p / 3)
-
-    if (0 >= loc):
-        return (5.0)
-
-    return -0.75 * (np.log(loc))
+    else:
+        loc = 1 - (4 * p / 3)
+        if (0 >= loc):
+            return  5.0
+        else:
+            return -0.75*np.log(loc)
