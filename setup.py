57d0c836f447dc90841cd74c807b914741eecd0a
Metin Balaban <metin@Metins-MacBook-Pro.local>
2019-09-10 16:51:53 -0700

    pip installable

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup,find_packages
+
+setup(
+        name='apples',    # This is the name of your PyPI-package.
+        version='1.2.3',    # Update the version number for new releases
+        scripts=['run_apples.py',], # The name of your scipt, and also the command you'll be using for calling it
+        description='APPLES: a distance-based phylogenetic placement tool',
+        long_description='APPLES stands for Accurate Phylogenetic Placement\
+            with LEast Squares and addresses the problem of phylogenetic placement\
+            of DNA and protein sequences into an already existing reference tree.',
+        long_description_content_type='text/plain',
+        url='https://github.com/balabanmetin/apples',
+        author='Metin Balaban',
+        author_email='balaban@ucsd.edu',
+        packages=find_packages(),
+        zip_safe = False,
+        install_requires=['numpy','treeswift'],
+        include_package_data=True
+)
